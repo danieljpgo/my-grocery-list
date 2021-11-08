2bd5c994cba5c3f79553b90167de6dc6f6860093
Daniel Jorge <djpgonzaga@gmail.com>
2021-11-08 01:53:47 -0300

    fix: absolute components path

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Container } from '../common/components';
+import { GroceryProvider } from '~/context/groceryContext';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -14,19 +15,15 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         /> */}
         <meta name="theme-color" content="#fff" />
-        <meta
-          name="apple-mobile-web-app-status-bar-style"
-          content="black-translucent"
-        />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="viewport" content="initial-scale=1, viewport-fit=cover" />
-        <meta
-          name="viewport"
-          content="initial-scale=1, viewport-fit=cover, user-scalable=no"
-        />
+        <meta name="viewport" content="initial-scale=1, viewport-fit=cover, user-scalable=no" />
       </Head>
       <ChakraProvider>
         <Container>
-          <Component {...pageProps} />
+          <GroceryProvider>
+            <Component {...pageProps} />
+          </GroceryProvider>
         </Container>
       </ChakraProvider>
     </>
